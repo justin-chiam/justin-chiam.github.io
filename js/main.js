4d9c0c03a81510741bfeb9fd2a831f62b9c4fd30
Justin Chiam <235694260+justin-chiam@users.noreply.github.com>
2026-08-24 10:14:43 +1000

    feat: add main section to home

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+function updateClock() {
+  const clockElement = document.getElementById('clock');
+  const now = new Date();
 
+  const timeString = now.toLocaleTimeString('en-AU', {
+    timeZone: 'Australia/Sydney',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  });
+
+  const formattedTime = timeString.replace(/\s/g, '').toUpperCase();
+  
+  clockElement.textContent = `${formattedTime} AEST`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
