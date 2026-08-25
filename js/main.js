57e433ffc6c96829be0ff22a1c6b3a3d6d768cc6
Justin Chiam <235694260+justin-chiam@users.noreply.github.com>
2026-08-25 16:36:33 +1000

    feat: add slider button for night/day toggle
    adapted from my previous tp application submission

--- js/main.js
+++ js/main.js
@@ -14,5 +14,25 @@ function updateClock() {
   clockElement.textContent = `${formattedTime} AEST`;
 }
 
+function applySavedTheme() {
+    const isNightMode = localStorage.getItem("theme") === "night";
+    
+    // Toggle night mode if saved and check button
+    document.body.classList.toggle("night-mode", isNightMode);
+    document.getElementById("theme-toggle").checked = isNightMode;
+}
+
+function toggleTheme() {
+    const isNightMode = document.body.classList.toggle("night-mode");
+
+    // Set theme in local storage
+    if (isNightMode) {
+        localStorage.setItem("theme", "night");
+    } else {
+        localStorage.setItem("theme", "day");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", applySavedTheme);
 updateClock();
 setInterval(updateClock, 1000);
